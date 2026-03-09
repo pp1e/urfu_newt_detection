1b7a4f71a042f35ab198495e418fcc0d730ffbec
denis <malyutin.02@bk.ru>
2026-03-09 19:28:14 +0300

    Add ability to set up proxy for aiogram

--- tg_bot/bot/bot.py
+++ tg_bot/bot/bot.py
@@ -2,8 +2,12 @@ from aiogram import Bot, Dispatcher
 from aiogram.client.session.aiohttp import AiohttpSession
 
 from settings.loader import BOT_TOKEN
+from settings.network_config import PROXY_URL
 
-session = AiohttpSession(timeout=120)
+session = AiohttpSession(
+    timeout=120,
+    proxy=PROXY_URL,
+)
 
 bot = Bot(
     token=BOT_TOKEN,
---
+++ tg_bot/settings/network_config.py
@@ -0,0 +1,7 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+PROXY_URL = os.getenv("PROXY_URL", "")
